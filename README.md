e1104c7b3d0870e02db32b67f2a2eb0268679fff
Miguel Angel Gordian <os.aioria@gmail.com>
2014-11-26 14:00:45 -0600

    Feature: List Container and Images

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # Jocker
 
 [![Build Status](https://travis-ci.org/zoek1/Jocker.jl.svg?branch=master)](https://travis-ci.org/zoek1/Jocker.jl)
+
+An API client for docker writen in Julia.
+
+This package is still under initial development and could be changed
+drastically.
+
+## Instalation
+
+```julia
+Pkg.clone("https://github.com/zoek1/Jocket.git")
+```
+
+## Documentation
+
+The documentation will be available in ReadTheDocs as soon as possible.
+Also sources are available in the `docs` directory.
+
+## License
+
+Docker is licensed under the MIT License. See LICENSE.md for full license text.
--- src/Jocker.jl
+++ src/Jocker.jl
@@ -1,5 +1,49 @@
 module Jocker
 
-# package code goes here
+using Requests
+using JSON
 
-end # module
+export Mode, socket, remote, Component, Container, Image, Ip, Port,
+       Dock, lists, list
+
+abstract Mode
+abstract socket <: Mode
+abstract remote <: Mode
+
+abstract Component
+abstract Container <: Component
+abstract Image <: Component
+
+typealias Ip String
+typealias Port Unsigned
+
+
+const OK = 200
+
+immutable Dock
+  host :: Ip
+  port :: Port
+  mode :: Union(Type{remote}, Type{socket})
+end
+
+
+headers = { "Content-Type" => "application/json"}
+
+uri(conn::Dock, schema="http") = URI(schema, conn.host, conn.port)
+uri(conn::Dock, path::String, schema="http") = URI(schema, conn.host, conn.port, path)
+
+function list(e::Type{Container}, conn::Dock, query={"all" => 1})
+  r = get(uri(conn, "/containers/json"), query=query)
+  return r.status == OK ? JSON.parse(r.data) : []
+end
+
+function list(e::Type{Image}, conn::Dock, query={"all" => 1})
+  r = get(uri(conn, "/images/json"), query=query)
+  return r.status == OK ? JSON.parse(r.data) : []
+end
+
+end
+
+macro lists(e, con)
+  return :(list($e, $con))
+end
